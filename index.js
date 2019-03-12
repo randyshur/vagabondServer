2afa36b21e1f208aac12ce67e2a8a851639a87fa
randyshur <rjdev@shurvideo.com>
2019-03-12 10:22:16 -0400

    initial server configuration

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const sequelize = require('./db');
+const bodyParser = require('body-parser');
+
+// Need controllers
+// ex: const user = require('./controllers/usercontroller');
+
+sequelize.sync();
+app.use(bodyParser.json());
+app.use(require('./middleware/headers'));
+
+// Need api calls here 
+// ex: app.use('/api/user', user)
+
+app.get('/', (req, res) => res.sendfile('index.html'));
+
+app.listen(process.env.PORT,() => console.log(` ${process.env.NAME} is listening on ${process.env.PORT}`));
