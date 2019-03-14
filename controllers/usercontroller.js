1bb0e217521873fdfe09f38bcc3545056d260a90
randyshur <rjdev@shurvideo.com>
2019-03-14 13:40:33 -0400

    maddie quinn major merge

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -3,22 +3,21 @@ const User = require('../db').import('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
-
 // Sign up a new user
 router.post('/signup', (req, res) => {
 
   User.create({
-    username: req.body.user.username,
-    password: bcrypt.hashSync(req.body.user.password, 10),
-    firstName: req.body.user.firstName,
-    lastName: req.body.user.lastName,
-    email: req.body.user.email,
-    address: req.body.user.address,
-    city: req.body.user.city,
-    state: req.body.user.state,
-    zip: req.body.user.zip,
-    latitude: req.body.user.latitude,
-    longitude: req.body.user.longitude
+    username: req.body.username,
+    password: bcrypt.hashSync(req.body.password, 10),
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    address: req.body.address,
+    city: req.body.city,
+    state: req.body.state,
+    zip: req.body.zip,
+    latitude: req.body.latitude,
+    longitude: req.body.longitude
   })
     .then(
       createSuccess = (user) => {
@@ -38,12 +37,12 @@ router.post('/signup', (req, res) => {
 // Sign in user
 router.post('/signin', function (req, res) {
 
-  User.findOne({ where: { username: req.body.user.username } }
+  User.findOne({ where: { username: req.body.username } }
   ).then(
     function (user) {
       if (user) {
 
-        bcrypt.compare(req.body.user.password, user.password, function (err, matches) {
+        bcrypt.compare(req.body.password, user.password, function (err, matches) {
           if (matches) {
             var token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 24 });
             res.json({ user: user, message: 'successfully authenticated', sessionToken: token });
@@ -79,7 +78,7 @@ router.get('/id/:id', (req, res) => {
 // Update Users
 router.put('/:id', (req, res) => {
   if (!req.errors) {
-    User.update(req.body.user, { where: { id: req.params.id }})
+    User.update(req.body, { where: { id: req.params.id }})
       .then(user => res.status(200).json(user))
       .catch(err => res.json(err))
   } else {
