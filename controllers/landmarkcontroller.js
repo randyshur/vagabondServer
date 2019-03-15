ca852e2c442fa7ca867ba4aeb162f45e9cbe4a40
randyshur <rjdev@shurvideo.com>
2019-03-15 16:15:40 -0400

    no associations

--- controllers/landmarkcontroller.js
+++ controllers/landmarkcontroller.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
-const User = require('../db').import('../models/user');
-const State = require('../db').import('../models/state');
 const Landmark = require('../db').import('../models/landmark');
-State.belongsTo(User);
-Landmark.belongsTo(State);
+
 
 // Create Landmark
 router.post('/', (req, res) => {
@@ -31,74 +28,6 @@ router.post('/', (req, res) => {
       createError = err => res.status(500).send(err)
     )
 
-})
-
-// Get all landmarks
-router.get('/', (req, res) => {
-  Landmark.findAll({ 
-    include: [
-      {
-        model: State,
-        include: [
-          {
-            model: User
-          }
-        ]
-      }
-    ]
-  })
-    .then(landmarks => res.status(200).json(landmarks))
-    .catch(err => res.status(500).json({ error: err }))
-});
-
-// Get all user landmarks
-router.get('/user/:userId', (req, res) => {
-  Landmark.findAll({ 
-    include: [
-      {
-        model: State,
-        where: {userId: req.params.userId},
-        include: [
-          {
-            model: User,
-            
-          }
-        ],
-        
-      }
-    ]
-  })
-    .then(landmarks => res.status(200).json(landmarks))
-    .catch(err => res.status(500).json({ error: err }))
-});
-
-// Get single landmark by id for updating
-router.get('/id/:id', (req, res) => {
-  Landmark.findOne({where: {id: req.params.id}})
-  .then(landmark => res.status(200).json(landmark))
-  .catch(err => res.status(500).json(err))
-});
-
-// Update landmark
-router.put('/:id', (req, res) => {
-  if (!req.errors) {
-    Landmark.update(req.body.landmark, { where: { id: req.params.id }})
-      .then(landmark => res.status(200).json(landmark))
-      .catch(err => res.json(err))
-  } else {
-    res.status(500).json(req.errors)
-  }
-})
-
-// Delete state
-router.delete('/:id', (req, res) => {
-  if (!req.errors) {
-    Landmark.destroy({ where: { id: req.params.id }})
-      .then(landmark => res.status(200).json(landmark))
-      .catch(err => res.json(req.errors))
-  } else {
-    res.status(500).json(req.errors)
-  }
 })
 
 module.exports = router;
--- controllers/statecontroller.js
+++ controllers/statecontroller.js
@@ -1,18 +1,14 @@
 const router = require('express').Router();
-const User = require('../db').import('../models/user');
 const State = require('../db').import('../models/state');
-const validateSession = require('../middleware/validate-session')
-var sequelize = require('sequelize')
-State.belongsTo(User);
 
 // Create state for curren tuser
-router.post('/mine', validateSession, (req, res) => {
+router.post('/',  (req, res) => {
 
   State.create({
     state: req.body.state,
     dateLastVisited: req.body.dateLastVisited,
     comments: req.body.comments,
-    userId: req.user.id
+    userId: req.body.id
   })
     .then(
       createSuccess = (state) => {
@@ -25,56 +21,6 @@ router.post('/mine', validateSession, (req, res) => {
       createError = err => res.status(500).send(err)
     )
 
-})
-
-// Get all states for current user
-router.get('/mine', validateSession, (req, res) => {
-  State.findOne({where: {id: req.user.id}})
-  .then(state => res.status(200).json(state))
-  .catch(err => res.status(500).json(err))
-});
-
-// Get all states
-router.get('/', (req, res) => {
-  State.findAll()
-    .then(states => res.status(200).json(states))
-    .catch(err => res.status(500).json({ error: err }))
-});
-
-// Search to validate is userid-state combination exists
-router.get('/validate/:state/:userId', function(req, res) {
-  State.findAll({where: {state: req.params.state, userId: req.params.userId}})
-  .then(state => res.status(200).json(state))
-  .catch(err => res.status(500).json(err))
-});
-
-// Get unique states for dropdowns
-router.get('/unique', (req, res) => {
-  State.findAll({ attributes: [[sequelize.fn('DISTINCT', sequelize.col('state')), 'state']] })
-    .then(states => res.status(200).json(states))
-    .catch(err => res.status(500).json({ error: err }))
-});
-
-// Update states
-router.put('/:id', (req, res) => {
-  if (!req.errors) {
-    State.update(req.body.state, { where: { id: req.params.id }})
-      .then(state => res.status(200).json(state))
-      .catch(err => res.json(err))
-  } else {
-    res.status(500).json(req.errors)
-  }
-})
-
-// Delete state
-router.delete('/:id', (req, res) => {
-  if (!req.errors) {
-    State.destroy({ where: { id: req.params.id }})
-      .then(state => res.status(200).json(state))
-      .catch(err => res.json(req.errors))
-  } else {
-    res.status(500).json(req.errors)
-  }
 })
 
 module.exports = router;
